c1f6ef9d4d3a2d850ce8b1bbe6dd3dc11fb2246e
Pauline Duterme <pdutermetumbarello@gmail.com>
2026-05-22 10:59:11 +0200

    feat: add HTML rendering and CSS styling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Bienvenue sur le forum !")
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+
+	if err != nil {
+		http.Error(w, "Erreur chargement page", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", homeHandler)
 
-	fmt.Println("Serveur lancé sur http://localhost:8080")
+	log.Println("Serveur lancé sur http://localhost:8080")
 
 	err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
-		fmt.Println("Erreur serveur :", err)
+		log.Fatal(err)
 	}
 }
